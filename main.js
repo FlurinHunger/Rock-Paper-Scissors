721f8d45872d742c4efd51ddbad4cf55df7f999d
FlurinHunger <47223054+FlurinHunger@users.noreply.github.com>
2022-09-26 01:58:27 +0200

    basic functionality

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ let userScore = 0;
 let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
-const scoreBoard_div = document.getElementById(".score-board");
-const result_div = document.getElementById(".result");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
@@ -15,22 +15,40 @@ function getComputerChoice() {
   return choices[randomNumber];
 }
 
-function win() {
-  console.log("WIN");
+function convertToWord(letter) {
+  if (letter === "r") return "Rock";
+  if (letter === "p") return "Paper";
+  return "Scissors";
+}
+
+function win(userChoice, computerChoice) {
+  const smallUserWord = "user".fontsize(3).sup();
+  const smallCompWord = "comp".fontsize(3).sup();
   userScore++;
-  userScore_span = userScore;
-  console.log(userScore);
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats ${convertToWord(computerChoice)}${smallCompWord}. You win!`;
+  document.getElementById(userChoice).classList.add('green-glow');
+  setTimeout(() => document.getElementById(userChoice).classList.remove('green-glow'), 500);
 }
 
-function lose() {
-  console.log("LOSE");
+function lose(userChoice, computerChoice) {
+  const smallUserWord = "user".fontsize(3).sup();
+  const smallCompWord = "comp".fontsize(3).sup();
   computerScore++;
-  computerScore_span = computerScore;
-  console.log(computerScore);
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} loses to ${convertToWord(computerChoice)}${smallCompWord}. You lost!`;
+  document.getElementById(userChoice).classList.add('red-glow');
+  setTimeout(() => document.getElementById(userChoice).classList.remove('red-glow'), 500);
 }
 
-function draw() {
-  console.log("DRAW");
+function draw(userChoice, computerChoice) {
+  const smallUserWord = "user".fontsize(3).sup();
+  const smallCompWord = "comp".fontsize(3).sup();
+  result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} equals ${convertToWord(computerChoice)}${smallCompWord}. Its a draw!`;
+  document.getElementById(userChoice).classList.add('grey-glow');
+  setTimeout(() => document.getElementById(userChoice).classList.remove('grey-glow'), 500);
 }
 
 function game(userChoice) {
@@ -39,19 +57,19 @@ function game(userChoice) {
     case "rp":
     case "ps":
     case "sr":
-      lose();
+      lose(userChoice, computerChoice);
       break;
 
     case "pr":
     case "rs":
     case "sp":
-      win();
+      win(userChoice, computerChoice);
       break;
 
     case "ss":
     case "pp":
     case "rr":
-      draw();
+      draw(userChoice, computerChoice);
       break;
   }
 }
