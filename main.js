5d7d9df1a7a207a858b5d7a122acad973b0bdcea
FlurinHunger <47223054+FlurinHunger@users.noreply.github.com>
2022-09-26 00:37:57 +0200

    d

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.getElementById(".score-board");
@@ -8,7 +8,66 @@ const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
-while (userScore <= 10) {
-  userScore = userScore + 1;
+
+function getComputerChoice() {
+  const choices = ['r', 'p', 's'];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
+}
+
+function win() {
+  console.log("WIN");
+  userScore++;
+  userScore_span = userScore;
   console.log(userScore);
 }
+
+function lose() {
+  console.log("LOSE");
+  computerScore++;
+  computerScore_span = computerScore;
+  console.log(computerScore);
+}
+
+function draw() {
+  console.log("DRAW");
+}
+
+function game(userChoice) {
+  const computerChoice = getComputerChoice();
+  switch (userChoice + computerChoice) {
+    case "rp":
+    case "ps":
+    case "sr":
+      lose();
+      break;
+
+    case "pr":
+    case "rs":
+    case "sp":
+      win();
+      break;
+
+    case "ss":
+    case "pp":
+    case "rr":
+      draw();
+      break;
+  }
+}
+
+function main() {
+  rock_div.addEventListener('click', function() {
+    game("r");
+  })
+
+  paper_div.addEventListener('click', function() {
+    game("p");
+  })
+
+  scissors_div.addEventListener('click', function() {
+    game("s");
+  })
+}
+
+main();
